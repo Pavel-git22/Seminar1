e3bfd3fc3a65d605c77f7a2d64d475679fa43882
Павел <p.dolmatov75@gmail.com>
2022-12-17 23:27:37 +0300

    первый алгоритм

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" input number:  ");
+int x= Convert.ToInt32(Console.ReadLine());
+int y = x * x ;
+Console.WriteLine($" quard to {x}   --> {y}" );
+
